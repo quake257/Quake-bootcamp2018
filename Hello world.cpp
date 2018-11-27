1f376a56783d1c7ecca54b64b86b8f7a32786f76
quake257 <45363314+quake257@users.noreply.github.com>
2018-11-27 09:29:20 +0100

    Hello World
    1ts file added

---
+++ Hello world.cpp
@@ -0,0 +1,9 @@
+#include <iostream>								// ukljucivanje funkcija za input/output
+
+using namespace std;							// definiranje spektra imena za sve naredne funkcije
+
+int main(){										// definiranje glavnog funkcije
+	cout << "Hello World!" << endl;				// ispis teksta i prelazak u novi red
+	system("PAUSE");							// pauza da se conzola ne ugasi
+	return 0;									// povratak sustavu informacije o uspjesnom izvrsenju programa
+}
